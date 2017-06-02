e474b0cb36f5f7cd7d799be0e905ebc495f95cfd
Noteee <lugosi.norbert@gmail.com>
2017-06-02 10:45:04 +0200

    Add project files.

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Person
+    {
+        public String Name { get; set; }
+        public int BirthDate { get; set; }
+
+
+        public Person()
+        {
+
+        }
+
+        public Person (String Name, int BirthDate)
+        {
+            this.Name = Name;
+            this.BirthDate = BirthDate;
+        }
+
+        public enum Genders : int { Male, Female };
+
+        public Genders gender;
+
+        public override string ToString()
+        {
+            return $"name: {Name}, birth date: {BirthDate}";
+        }
+    }
+}
